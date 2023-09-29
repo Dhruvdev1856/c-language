b158086c8cd65b357335aff1ceb3f5aaa5aaee52
Dhruv <dhruvdev1856@gmail.com>
2023-09-29 14:25:28 +0530

    simple for loop

---
+++ for2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i=0;
+    for(i=0;i<=10;i++)
+    {
+        printf("\n i=%d",i);
+    }
+    printf("\n value of i outside loop %d" , i);
+    return 0;
+}
+
+
