2fc5abcee63bbf2d53b1f67935b42552bb3d2c75
Dhruv <dhruvdev1856@gmail.com>
2023-09-29 14:27:29 +0530

    decrement order for loop

---
+++ for3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i=0;
+    for(i=10;i>=0;i--)
+    {
+        printf("\n i=%d",i);
+    }
+    printf("\n value of i outside loop %d" , i);
+    return 0;
+}
