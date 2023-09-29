b8a8f0fa48ebed468b5eae511f4892453813485b
Dhruv <dhruvdev1856@gmail.com>
2023-09-29 10:35:17 +0530

    simple division

---
+++ div1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    float a=1,b=2,c; // 4 byte data type and .000000 clrscr();
+    c=a/b;
+    printf("\n Answer of %.2f / %.2f = %.2f",a,b,c);
+    return 0;
+}
