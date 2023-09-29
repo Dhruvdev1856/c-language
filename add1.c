0504840a49295ca9053ab2cb2b82472b2f5ffa73
Dhruv <dhruvdev1856@gmail.com>
2023-09-29 10:38:35 +0530

    simple addition

---
+++ add1.c
@@ -0,0 +1,12 @@
+#include<stdio.h> // standard input output. header file 
+#include<conio.h> // consol input output. header file
+
+int main()
+{
+    int a,b,ans;  // %d %i
+    a=80;
+    b=20;
+    ans=a+b;
+    printf("%d + %d = %d",a,b,ans);
+    return 0;
+}
