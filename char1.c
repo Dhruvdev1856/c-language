a482e2f4513d26ccd275e57f2a63b490c3d99b94
Dhruv <dhruvdev1856@gmail.com>
2023-09-29 13:29:45 +0530

    show character and ascii value

---
+++ char1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char x;
+    printf("Enter any char:");
+    scanf("%c",&x); // & = ampersand
+    printf("\nThe entered char = %c and its ASCII id %d",x,x); //A = 65
+    return 0;
+}
