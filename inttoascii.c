cef0888e2b5fb9564136738996567a0580810ee9
Dhruv <dhruvdev1856@gmail.com>
2023-09-29 13:59:04 +0530

    finding character from its ascii value

---
+++ inttoascii.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int n;
+    printf("Enter any value:");
+    scanf("%i", &n);
+    printf("\nThe equivalent char of %d is %c",n,n);
+    return 0;
+}
