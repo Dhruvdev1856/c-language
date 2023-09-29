4829b745bf3922153eff24926e0881a242854c09
Dhruv <dhruvdev1856@gmail.com>
2023-09-29 14:05:10 +0530

    first use of do while

---
+++ dowhile1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i=1;
+    do
+    {
+    // exit control loop
+    printf("%d ",i++); // ++i = preincrement and i++ postincrement while(i
+    }while(i<=5);
+    return 0;
+}
