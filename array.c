d2279c5803e901c050dee417e145e4d169ee9666
Dhruv <dhruvdev1856@gmail.com>
2023-10-05 07:48:37 +0530

    how address is given to array elements

--- array.c
+++ array.c
@@ -7,5 +7,10 @@ int main()
     //there are two types of arrays
     //single dimension
     //double dimention (2d,3d,.....,nd)
+    int a[3],i;
+    for(i=0;i<=2;i++)
+    {
+        printf("\n%u ",&a[i]); // address printing
+    }
     return 0;
 }
