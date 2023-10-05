5673578d657218a91aeca8f8bf041d81bfb14097
Dhruv <dhruvdev1856@gmail.com>
2023-10-05 07:43:31 +0530

    array defination

---
+++ array.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    //array : collection of elements having similar data types.
+    //there are two types of arrays
+    //single dimension
+    //double dimention (2d,3d,.....,nd)
+    return 0;
+}
